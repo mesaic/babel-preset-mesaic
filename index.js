639bb4901df252c0aa7bf10a90fdf74acab188b0
Jonathan Werner <jwerner@posteo.de>
2017-03-06 10:59:24 +0100

    add index

---
+++ index.js
@@ -0,0 +1,14 @@
+module.exports = function buildMesaicPreset(context, options) {
+  let preset; // eslint-disable-line no-var
+  return {
+    presets: [
+      require('babel-preset-env'),
+      require('babel-preset-stage-0'),
+      require('babel-preset-react'),
+    ],
+    plugins: [
+      require('babel-plugin-react-autoprefix'),
+      require('babel-plugin-transform-runtime'),
+    ],
+  };
+};
